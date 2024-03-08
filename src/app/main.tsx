1fbd88d9ef3e4af66f6208e2d59c97d51888cac7
loveuloveme <olegpodkolzin@icloud.com>
2024-03-08 08:40:52 +0300

    feat: vk bridge

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,22 +1,28 @@
 import { createRoot } from 'react-dom/client';
-
 import { App } from './app';
-
 import { AdaptivityProvider, AppRoot, ConfigProvider } from '@vkontakte/vkui';
 import { router } from '@/pages/router';
 import { RouterProvider } from '@vkontakte/vk-mini-apps-router';
+import bridge from '@vkontakte/vk-bridge';
 
-const container = document.querySelector('#root') as HTMLElement;
-const root = createRoot(container);
 
-root.render(
-    <ConfigProvider>
-        <AdaptivityProvider>
-            <AppRoot>
-                <RouterProvider router={router}>
-                    <App />
-                </RouterProvider>
-            </AppRoot>
-        </AdaptivityProvider>
-    </ConfigProvider>,
-);
+
+const main = () => {
+    const container = document.querySelector('#root') as HTMLElement;
+    const root = createRoot(container);
+
+    root.render(
+        <ConfigProvider>
+            <AdaptivityProvider>
+                <AppRoot>
+                    <RouterProvider router={router}>
+                        <App />
+                    </RouterProvider>
+                </AppRoot>
+            </AdaptivityProvider>
+        </ConfigProvider>,
+    );
+};
+
+
+bridge.send('VKWebAppInit').finally(main);
